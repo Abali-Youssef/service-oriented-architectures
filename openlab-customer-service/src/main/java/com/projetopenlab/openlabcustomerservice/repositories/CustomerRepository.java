376670481e8db95277de10632979da8da64cc002
Abali-Youssef <79579032+Abali-Youssef@users.noreply.github.com>
2022-10-23 23:58:12 +0100

    Delete openlab-customer-service directory

--- openlab-customer-service/src/main/java/com/projetopenlab/openlabcustomerservice/repositories/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projetopenlab.openlabcustomerservice.repositories;
-
-import com.projetopenlab.openlabcustomerservice.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,String> {
-}
