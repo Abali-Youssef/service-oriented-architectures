405e4164a8ea774fe6750f474cb987e5d669f344
abali-youssef <youssabali486@gmail.com>
2022-12-13 23:22:51 +0100

    billing-service: vault

--- activity-4/billing/src/main/java/com/projet/billing/ConsulConfigRestController.java
+++ activity-4/billing/src/main/java/com/projet/billing/ConsulConfigRestController.java
@@ -9,15 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RefreshScope
-public class ConsulConfigRestController {
 
-    @Value("${token.accessTokenTimeout}")
-    private long accessTokenTimeout;
-    @Value("${token.refreshTokenTimeout}")
-    private long refreshTokenTimeout;
+public class ConsulConfigRestController {
+    @Autowired
+    private MyConsulConfig myConsulConfig;
+    @Autowired
+    private MyVaultConfig myVaultConfig;
+    //@Value("${token.accessTokenTimeout}")
+    //private long accessTokenTimeout;
+    //@Value("${token.refreshTokenTimeout}")
+    //private long refreshTokenTimeout;
     @GetMapping("/myConfig")
     public Map<String,Object> myConfig(){
-        return Map.of("refreshTokenTimeout",refreshTokenTimeout, "accessTokenTimeout",accessTokenTimeout);
+        return Map.of("consulConfig",myConsulConfig, "vaultConfig",myVaultConfig);
     }
 }
